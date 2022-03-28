54eef5b04f64defa9893dcc79bea2e36c3d2adb0
kushalnigam0604 <nigamkushal0@gmail.com>
2022-03-28 22:30:50 +0530

    Add js

---
+++ index.js
@@ -0,0 +1,47 @@
+showNotes();
+
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function (e) {
+  let addTextarea = document.getElementById("addTextarea");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTextarea.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTextarea.value = "";
+  console.log(notesObj);
+  showNotes();
+});
+
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html =
+      html +
+      `
+        <div class="mx-2 my-2 card" style="width: 18rem">
+        <div class="card-body">
+          <h5 class="card-title">Note ${index + 1}</h5>
+            <p class="card-text">${element}</p>
+          </div>
+          <button class="btn btn-primary" id="addBtn">Delete Note</button>
+        </div>
+      </div>`;
+  });
+  let notesElm = document.getElementById("notes");
+  if(notesObj.length!= 0){
+      notesElm.innerHTML = html;
+  }
+  else{
+      notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
+  }
+}
